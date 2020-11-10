7b10ad6d7e850a01c79681f93f33ae5fbd8b7663
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2020-11-10 21:50:10 +0900

    Create  StandardNormalDistribution.cs

---
+++ StandardNormalDistribution.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace _A.Properties
+{
+    public class StandardNormalDistribution
+    {
+        static void Main(string[] args) {
+            Dictionary<double, double> Zlist = new Dictionary<double, double>();
+            Zlist.Add(0.00, 0.000);
+            Zlist.Add(0.01, 0.040);
+            Zlist.Add(0.02, 0.080);
+            Zlist.Add(0.03, 0.120);
+            Zlist.Add(0.04, 0.160);
+
+        }
+    }
+}
