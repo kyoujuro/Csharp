404058fc3054d1dc8771487386159ab739383859
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2020-11-11 06:35:50 +0900

    Update StandardNormalDistribution.cs

--- StandardNormalDistribution.cs
+++ StandardNormalDistribution.cs
@@ -12,6 +12,8 @@ namespace _A.Properties
             Zlist.Add(0.02, 0.080);
             Zlist.Add(0.03, 0.120);
             Zlist.Add(0.04, 0.160);
+            Zlist.Add(0.05, 0.199);
+            Zlist.Add(0.06, 0.239);
 
         }
     }
