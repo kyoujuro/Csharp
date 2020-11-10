3958a902574ea3dcc06b1703d28732e1ffa5b6c0
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2020-11-11 06:43:04 +0900

    Update StandardNormalDistribution.cs

--- StandardNormalDistribution.cs
+++ StandardNormalDistribution.cs
@@ -8,17 +8,20 @@ namespace _A.Properties
         static void Main(string[] args) {
             Dictionary<double, double> Zlist = new Dictionary<double, double>();
             Zlist.Add(0.00, 0.000);
-            Zlist.Add(0.01, 0.040);
-            Zlist.Add(0.02, 0.080);
-            Zlist.Add(0.03, 0.120);
-            Zlist.Add(0.04, 0.160);
-            Zlist.Add(0.05, 0.199);
-            Zlist.Add(0.06, 0.239);
-            Zlist.Add(0.07, 0.279);
-            Zlist.Add(0.08, 0.319);
-            Zlist.Add(0.09, 0.359);
-            Zlist.Add(0.10, 0.398);
-            Zlist.Add(0.11, 0.438);
+            Zlist.Add(0.01, 0.0040);
+            Zlist.Add(0.02, 0.0080);
+            Zlist.Add(0.03, 0.0120);
+            Zlist.Add(0.04, 0.0160);
+            Zlist.Add(0.05, 0.0199);
+            Zlist.Add(0.06, 0.0239);
+            Zlist.Add(0.07, 0.0279);
+            Zlist.Add(0.08, 0.0319);
+            Zlist.Add(0.09, 0.0359);
+            Zlist.Add(0.10, 0.0398);
+            Zlist.Add(0.11, 0.0438);
+            Zlist.Add(0.12, 0.0478);
+            Zlist.Add(0.13, 0.0517);
+            Zlist.Add(0.14, 0.0557);
         }
     }
 }
