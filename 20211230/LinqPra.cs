11900907b9b0f7ffb194b6559ad7480abdae2794
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2022-01-15 18:21:59 +0900

    add

---
+++ 20211230/LinqPra.cs
@@ -0,0 +1,9 @@
+var hoge = new[]
+{
+    new { id = "a", num = 20},
+    new { id = "b", num = 21},
+    new { id = "c", num = 22},
+    new { id = "d", num = 23},
+    new { id = "e", num = 24},
+}
+var sum = hoge.Sum(x => x.num);
